baf9a7a88c3720cf4f8b27c1e9a66f9f1f6f8fca
Luke8877 <luke.thompson996@gmail.com>
2024-12-18 16:00:16 -0700

    Reverted changes

--- js/script.js
+++ js/script.js
@@ -52,11 +52,4 @@ document.getElementById("pollForm").addEventListener("submit", function (e) {
     thankYouModal.show();
 });
 
-// Sidebar toggle functionality
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('active'); 
-});
 
